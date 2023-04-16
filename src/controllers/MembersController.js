cb870041bd1cf7c65fdc2d72c9c35ebae4284f08
Milli-Saved <million12.tenkir@gmail.com>
2023-04-16 14:13:21 +0300

    user functions moved to members controller.

--- src/controllers/MembersController.js
+++ src/controllers/MembersController.js
@@ -1,26 +1,99 @@
 const asycnHandler = require("express-async-handler");
 const Member = require("../models/membersModel");
 
+// Generating JWT
+const generateToken = (id) => jwt.sign({ id }, "Marcil", { expiresIn: "1d" });
+
+// Register member
 const addNewMember = asycnHandler(async (req, res) => {
-  let member = await Member.create(req.body);
+  //checking username and password is not used.
+  const userNameExists = await User.findOne({ username });
+  const phoneNumberExists = await User.findOne({ phonenumber });
+
+  if (userNameExists) {
+    res.status(409);
+    throw new Error(`User name ${username} is already used.`);
+  }
+  if (phoneNumberExists) {
+    res.status(409);
+    throw new Error(`Phone number ${phonenumber} is already used.`);
+  }
+
+  // Hashing the Password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  let member = await Member.create({
+    firstName,
+    middleName,
+    lastName,
+    dateOfBirth,
+    userName,
+    password: hashedPassword,
+    roles,
+    phoneNumber,
+  });
   if (member) {
-    res.status(200).json(member);
+    res.status(201).json({
+      _id: member.id,
+      firstName: member.firstName,
+      middleName: member.middleName,
+      lastName: member.lastName,
+      dateOfBirth: member.dateOfBirth,
+      userName: member.userName,
+      roles: member.roles,
+      phoneNumber: member.phoneNumber,
+      token: generateToken(member._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid Member data.");
   }
 });
 
+// Login
+const login = asycnHandler(async (req, res) => {
+  const { username, password } = req.body;
+  const user = await Member.findOne({ username });
+  if (user) {
+    let checkPassword = await bcrypt.compare(password, user.password);
+    if (checkPassword) {
+      res.status(200).json({
+        _id: user._id,
+        username: user.username,
+        phonenumber: user.phonenumber,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error("Incorrect Password.");
+    }
+  } else {
+    res.status(400);
+    throw new Error("Incorrect Username.");
+  }
+});
+
+// Update member
 const updateMemberProfile = asycnHandler(async (req, res) => {
   const member = await Member.findById(req.params.id);
   if (!member) {
     res.status(400);
     throw new Error("Can not find the requested member.");
   }
-  
-  const updateMember = await Member.findByIdAndUpdate(req.params.id, req.body, {
+
+  let hashedPassword;
+  if ("password" in req.body) {
+    const salt = await bcrypt.genSalt(10);
+    hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword;
+  }
+  const updatedUser = await Member.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   });
-  res.status(200).json(updateMember);
+  res.status(200).json(updatedUser);
 });
 
+// Delete member
 const deleteMember = asycnHandler(async (req, res) => {
   const member = await Member.findById(req.params.id);
   if (!member) {
@@ -31,6 +104,7 @@ const deleteMember = asycnHandler(async (req, res) => {
   res.status(200).json({ msg: "Member is deleted." });
 });
 
+// Get all members
 const getAllMembers = asycnHandler(async (req, res) => {
   const member = await Member.find({});
   if (member) {
@@ -40,14 +114,17 @@ const getAllMembers = asycnHandler(async (req, res) => {
   }
 });
 
+// Get one member
 const getOneMember = asycnHandler(async (req, res) => {
   const member = await Member.findById(req.params.id);
   if (member) {
     res.status(200).json({ member });
   }
 });
+
 module.exports = {
   addNewMember,
+  login,
   updateMemberProfile,
   deleteMember,
   getAllMembers,
