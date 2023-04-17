0438183a4c0ea19586786878aaac086c13d59ca2
Milli-saved <million12.tenkir@gmail.com>
2023-04-17 11:11:44 +0300

    Programs model is created and controllers are underway.

--- src/models/programsModel.js
+++ src/models/programsModel.js
@@ -1,5 +1,20 @@
 const mongoose = require("mongoose");
 
-const programsSchema = mongoose.Schema({}, { timestamps: true });
+const programsSchema = mongoose.Schema(
+  {
+    programName: {
+      type: String,
+    },
+    department: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Department",
+    },
+    attendedMembers: [{ type: mongoose.Schema.Types.ObjectId, ref: "Member" }],
+    programDate: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Program", programsSchema);
