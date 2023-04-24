54ce07d193f5c355ea9051bba8eb818807dae143
Milli-saved <million12.tenkir@gmail.com>
2023-04-24 10:43:47 +0300

    church branch model is set yp

--- src/models/churchBranchModel.js
+++ src/models/churchBranchModel.js
@@ -1,5 +1,23 @@
 const mongoose = require("mongoose");
 
-const churchBranchSchema = mongoose.Schema({});
+const churchBranchSchema = mongoose.Schema({
+  churchName: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Church",
+  },
+  churchBranchName: {
+    type: String,
+  },
+  churchBranchLocation: {
+    type: String,
+  },
+  churchBranchLeadPastor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Member",
+  },
+  churchMembersNumber: {
+    type: Number,
+  },
+});
 
 module.exports = mongoose.model("ChurchBranch", churchBranchSchema);
